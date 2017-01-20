2e771046f7d7a0926eb45627232b9b6779dcaa83
Khaimov Irismet <iriska_4@bk.ru>
2017-01-20 14:41:51 +0600

    Changed AdminBCController

--- src/AppBundle/Controller/PageMenuController.php
+++ src/AppBundle/Controller/PageMenuController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\CustomSessionHandler;
+use Symfony\Component\HttpFoundation\Session\Storage\Proxy\SessionHandlerProxy;
 
 class PageMenuController extends Controller
 {
@@ -12,21 +15,26 @@ class PageMenuController extends Controller
      */
     public function menuAction()
     {
-			
-		if($this->get('security.authorization_checker')->isGranted('ROLE_LEASER'))
-		
+    	//$session = $this->getRequest()->getSession();
+    if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY'))
+    //if($session->has('login_gda_CMS'))
+    {
+    	if($this->get('security.authorization_checker')->isGranted('ROLE_LEASER'))
 		return $this->render('AppBundle:Pagemenu:leaser_menu.html.twig', array(
         ));
     	if($this->get('security.authorization_checker')->isGranted('ROLE_CHECKPOINT'))
 		return $this->render('AppBundle:Pagemenu:checkpoint_menu.html.twig', array(
         ));
-		if($this->get('security.authorization_checker')->isGranted('ROLE_SA'))
+	    if($this->get('security.authorization_checker')->isGranted('ROLE_SA'))
 		return $this->render('AppBundle:Pagemenu:admin_menu.html.twig', array(
         ));
 		if($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN'))
 		return $this->render('AppBundle:Pagemenu:adminbc_menu.html.twig', array(
         ));
-		
+	}
+		else if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_ANONYMOUS')) {
+			return $this->redirect('/login');
+		}
 		
     }
 
