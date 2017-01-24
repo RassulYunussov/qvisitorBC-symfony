84c4a3428dd662e1ba413f7871243f0929e95f9b
Aliya Akauova <alia__03@inbox.ru>
2017-01-24 12:48:31 +0600

    edited breadcrumbs checkpoint, form style

--- src/AppBundle/Form/qvHotEntranceType.php
+++ src/AppBundle/Form/qvHotEntranceType.php
@@ -7,6 +7,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 class qvHotEntranceType extends AbstractType
 {
     /**
@@ -16,17 +20,33 @@ class qvHotEntranceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('patronimic')
-            ->add('documentnumber')
-            ->add('organization')
-            ->add('attendant')
-            ->add('comment')
+            ->add('firstname', TextType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control'))
+            )
+            ->add('lastname', TextType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
+            ->add('patronimic', TextType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
+            ->add('documentnumber', NumberType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
+            ->add('organization', TextType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
+            ->add('attendant', TextType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
+            ->add('comment', TextareaType::class, array(
+                'attr'   =>  array(
+                'class'   => 'form-control')))
             ->add('entrancedate', DateTimeType::class, array(
                 'disabled' => 'true',
-            'placeholder' => array('datetime' => 'Datetime',))
-            )
+                'placeholder' => array('datetime' => 'Datetime',),
+                'attr' => array(
+                    'class' => 'type_date-inline' )))
             ->add('checkpoint')
             ->add('leaser')
             ->add('user')
