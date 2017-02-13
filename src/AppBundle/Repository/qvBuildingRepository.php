e62b57da03f35538b7449e347e630448fee8f0c7
Khaimov Irismet <iriska_4@bk.ru>
2017-02-13 16:07:42 +0600

    Changed headers and buttons in buildings control

--- src/AppBundle/Repository/qvBuildingRepository.php
+++ src/AppBundle/Repository/qvBuildingRepository.php
@@ -10,4 +10,18 @@ namespace AppBundle\Repository;
  */
 class qvBuildingRepository extends \Doctrine\ORM\EntityRepository
 {
+	/* public function countSectorInBuilding($build){
+     	return $this->getEntityManager()
+     	->createQuery(
+    		'SELECT COUNT(sector) FROM AppBundle:qvSector sector join sector.floor floor join floor.building build  
+    			WHERE build = :name'
+			)->setParameter('name', $build)->getSingleScalarResult();
+      }
+       public function countFloorInBuild($build){
+     	return $this->getEntityManager()
+     	->createQuery(
+    		'SELECT COUNT(fl) FROM AppBundle:qvFloor fl 
+    			WHERE fl.building = :name'
+			)->setParameter('name', $build)->getSingleScalarResult();
+      }*/
 }
